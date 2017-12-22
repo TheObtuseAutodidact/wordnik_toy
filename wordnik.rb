fa32f483678be537bf1fe0a1d1df90318ef05848
Obtuse <theobtuseautodidact@gmail.com>
2017-12-21 20:15:14 -0700

    Second iteration - whitespace was giving false results

--- wordnik.rb
+++ wordnik.rb
@@ -8,20 +8,20 @@ file = "test.txt"
 in_dict = "indict.txt"
 not_in_dict = "notindict.txt"
 
-f = File.open(file, "r")
 id = File.open(in_dict, "w")
 nid = File.open(not_in_dict, "w")
 
-
-f.each_line do |word|
-    if Wordnik.word.get_definitions(word, :source_dictionaries => "century")[0]
-      id.puts "#{word}: #{Wordnik.word.get_definitions(word, :source_dictionaries => "century")[0]["text"]}"
-    else
-      nid.puts word
+File.open('test.txt', 'r').each do |word|
+  unless Wordnik.word.get_definitions(word.strip, :source_dictionaries => "century").empty?
+    id.puts "#{word.strip}=\n"
+    Wordnik.word.get_definitions(word.strip, :source_dictionaries => "century").each do |definition|
+      id.puts "\t#{definition['text']}"
     end
+  else
+    nid.puts word
+  end
 end
 
-f.close
 id.close
 nid.close
 
